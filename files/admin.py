8ee403869cf92bd2f7077d65ed5b169c17735bd7
Владислав <kant1755@mail.ru>
2024-02-02 00:41:30 +0300

    create model File, admin panel

--- files/admin.py
+++ files/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import File
+
+"""
+Register the created models 
+for their processing by the administrator
+"""
+@admin.register(File)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('id', 'file', 'uploaded_at', 'processed',)
+    readonly_fields = ('uploaded_at', 'processed',)
+    list_filter = ('uploaded_at', 'processed',)
+    search_fields = ('uploaded_at',)
+    ordering = ('id', 'uploaded_at',)
+
--- files/models.py
+++ files/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class File (models.Model):
+    """
+    Class describing the fields of the "File" object 
+    in the database   
+    """
+    file = models.FileField(
+        upload_to='files_media',
+        verbose_name='Файл'        
+    )
+    uploaded_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата и время загрузки'
+    )
+    processed = models.BooleanField(
+        default=False,
+        verbose_name='Статус обработки файла'
+    )
+
+    class Meta():
+        verbose_name = 'Файл'
+        verbose_name_plural = 'Файлы'
