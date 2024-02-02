d8af95a143790c80f81a9dda758719faf0fc5581
Владислав <kant1755@mail.ru>
2024-02-03 00:26:54 +0300

    process_type_file

--- picasso/files/tasks.py
+++ picasso/files/tasks.py
@@ -1,5 +1,7 @@
 from celery import shared_task
+import mimetypes
 import logging
+import time
 
 logger = logging.getLogger(__name__)
 
@@ -15,8 +17,9 @@ def process_file(file_id):
         # Log entry about the start of task execution
         logger.info(f'Starting processing file {file_id}')
 
-        entity.processed = True
-        entity.save()
+        # Processing files depending on type
+        # example
+        process_type_file(entity)
 
         # Log entry about successful task completion
         return f'File id {entity.id} processed!'
@@ -24,3 +27,20 @@ def process_file(file_id):
         # Log about an error when executing a task
         logger.error(f'Error processing file {file_id}: {str(e)}')
         raise
+
+
+def process_type_file(entity):
+    """
+    Example of a function to process 
+    a file depending on its type
+    """    
+    file_type, _ = mimetypes.guess_type(entity.file.name)
+    logger.info(f'File has type: {file_type}')
+
+    # simulate a delay when processing a file
+    time.sleep(60)
+
+    # after the file type is determined
+    # the necessary processing can occur
+    entity.processed = True
+    entity.save()
